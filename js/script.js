bcee25b80ea0ee08ceb4e9153e4f252c0604a072
rei <sangraeryu@gmail.com>
2020-04-23 09:35:21 +1200

    javascript

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ const h_women = document.querySelector('.women');
 const l_men = document.querySelector('.menu_men');
 const l_women = document.querySelector('.menu_women');
 
-h_men.addEventListener('click', () => {
+h_men.addEventListener('mouseover', () => {
     l_men.classList.toggle('active');
-
 });
-
-h_women.addEventListener('click', () => {
+h_men.addEventListener('mouseout', () => {
+    l_men.classList.toggle('active');
+});
+h_women.addEventListener('mouseover', () => {
+    l_women.classList.toggle('active');
+});
+h_women.addEventListener('mouseout', () => {
     l_women.classList.toggle('active');
 });
